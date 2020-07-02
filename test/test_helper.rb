f5a78ec1cd2e576e9ede0f413a8269109d6c2929
aidewoode <aidewoode@gmail.com>
2020-07-02 16:29:08 +0800

    try to fix error of codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
 require 'simplecov'
-
-SimpleCov.start do
-  add_filter '/test/'
-end
 
 if ENV['CI'] == 'true'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
+SimpleCov.start do
+  add_filter '/test/'
+end
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'wahwah'
+require 'minitest/autorun'
+
 module TestHelpers
   def binary_data(file_path)
     File.read(file_path).force_encoding('BINARY').strip
   end
 end
-
-require 'wahwah'
-require 'minitest/autorun'
 
 Minitest::Test.send(:include, TestHelpers)
